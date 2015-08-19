5f7df51256226beb48808cb14345f2d1b371e728
Andy Situ <and.situ@gmail.com>
2015-08-19 16:44:51 -0700

    event.target.value in event handler

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function init() {
 	var select = document.getElementById("options");
 
 	function test(e) {
-		console.log(e);
+		console.log(e.target.value);
 	}
 
 	select.addEventListener("change", test, false); 
-
 	select = null;
+
 }
 
 window.onload = init;
