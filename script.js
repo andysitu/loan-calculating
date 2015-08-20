375afe9b7fde027329325bab6ab7b59305ee4d85
Andy Situ <and.situ@gmail.com>
2015-08-19 17:35:34 -0700

    submit but w/ event handler

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 function init() {
-	var select = document.getElementById("options");
-
 	function test(e) {
-		console.log(e.target.value);
+		console.log(e.target.value, typeof e.target.value);
 	}
 
+	var select = document.getElementById("options");
 	select.addEventListener("change", test, false); 
 	select = null;
 
+	function onSubmit(e) {
+		var bal = document.getElementById("balance");
+		console.log(e.target, bal.value);
+	}
+
+	var submit = document.getElementById("submit");
+	submit.addEventListener("click", onSubmit);
+	submit = null;
 }
 
 window.onload = init;
