2099687a153a808d09b5b41b2aa02c28eca1b6a1
Andy Situ <and.situ@gmail.com>
2015-08-19 19:57:04 -0700

    p obj with balance, rate, months set & get

---
+++ p.js
@@ -0,0 +1,23 @@
+var p = {
+	_balance: 0,
+	get balance() {
+		return this._balance;
+	},
+	set balance(value) {
+		this._balance = value;
+	},
+	_rate: 0,
+	get rate() {
+		return this._rate;
+	},
+	set rate(value) {
+		this._rate = value;
+	},
+	_months: 0,
+	get months() {
+		return this._months;
+	},
+	set months(value) {
+		this._months = value;
+	}
+};
