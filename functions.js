986d0d73bb871c1770f4ccd71c4aaab7eb0455ea
Andy Situ <and.situ@gmail.com>
2015-08-22 20:44:27 -0700

    moved functions in script to functions.js

---
+++ functions.js
@@ -0,0 +1,49 @@
+function display(msg) {
+	var msgArea = document.getElementById('msg');
+
+	msgArea.textContent = msg;
+	
+}
+
+function makeTable(dataArr, headerObj) {
+	/* Makes a table to doc by the headerObj (array) containing table heading
+	 * to specify order of the headings (optional) and dataObj which is objs 
+	 * in array that will contain the actual data.
+	 *
+	 * If headObj is not provided, then the headings might not be in order
+	 */
+	var docFrag = document.createDocumentFragment();
+
+	var keys = [];
+	if (Array.isArray(headerObj)) {
+		keys = headerObject.slice(0);
+	} else {
+		keys = Object.keys(dataArr[0]);
+	}
+
+	var tr = document.createElement("tr");
+	for (var i = 0; i < keys.length; i++) {
+		var th = document.createElement("th");
+		th.textContent = keys[i];
+		tr.appendChild(th);
+	}
+
+	docFrag.appendChild(tr);
+
+	for (i = 0; i < dataArr.length; i++) {
+		var tr = document.createElement("tr");
+		for (var j = 0; j < keys.length; j++) {
+			var td = document.createElement("td");
+			td.textContent = dataArr[i][keys[j]];
+			tr.appendChild(td);
+		}
+		docFrag.appendChild(tr);
+	}
+	
+	document.body.appendChild(docFrag);
+}
+
+function moneyFormatter(value) {
+	// Makes number into correct format for money
+	return Math.ceil(value * 100) / 100
+}
