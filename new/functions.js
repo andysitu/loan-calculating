b46e9427b7fd59bcea0ce8dd07b13717c92f0b2d
Andy Situ <and.situ@gmail.com>
2015-09-08 18:44:03 -0700

    commentize/ uncomment functions

--- new/functions.js
+++ new/functions.js
@@ -1,141 +1,141 @@
-function display(msg) {
-	var msgArea = document.getElementById('msg');
+// function display(msg) {
+// 	var msgArea = document.getElementById('msg');
 
-	msgArea.textContent = msg;
+// 	msgArea.textContent = msg;
 	
-}
+// }
 
-// setInput & clearInput are used when page is loaded
-// and local storage has prev info, so the inputs are
-// set as they are previously.
-function setInput(id, value) {
-	var input = document.getElementById(id);
+// // setInput & clearInput are used when page is loaded
+// // and local storage has prev info, so the inputs are
+// // set as they are previously.
+// function setInput(id, value) {
+// 	var input = document.getElementById(id);
 
-	input.value = value;
-}
+// 	input.value = value;
+// }
 
-function clearInput(id) {
-	setInput(id, "");
-}
+// function clearInput(id) {
+// 	setInput(id, "");
+// }
 
-function addClearEvents() {
-	function addClearEvent(id) {
-		// Sets clear event on ele with right click.
-		var ele = document.getElementById(id);
+// function addClearEvents() {
+// 	function addClearEvent(id) {
+// 		// Sets clear event on ele with right click.
+// 		var ele = document.getElementById(id);
 
-		ele.addEventListener("contextmenu", function(e){
-			e.preventDefault();
-			clearInput(id);
-		});
+// 		ele.addEventListener("contextmenu", function(e){
+// 			e.preventDefault();
+// 			clearInput(id);
+// 		});
 
-		ele = null;
-	}
+// 		ele = null;
+// 	}
 
-	addClearEvent("balanceInput");
-	addClearEvent("rateInput");
-	addClearEvent("monthsInput");
-	addClearEvent("paymentInput");
-}
+// 	addClearEvent("balanceInput");
+// 	addClearEvent("rateInput");
+// 	addClearEvent("monthsInput");
+// 	addClearEvent("paymentInput");
+// }
 
-function makeCircle() {
-/**
- * Makes circle with canvas that for now colors it in proportion
- *   amount of the total interest and the amount of balance. Also,
- *   it attaches itself to the canvas with id "canvas."
- */
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d"),
-		width = 700,
-		height = 320,
-		radius = 160,
-		pi = 2 * Math.PI,
-		startingBalance = p.getPayObjValue(1, "Starting Balance"),
-		totalInterest = p.getPayObjValue("end", "Total Interest"),
-		center = Math.floor(height/2);
+// function makeCircle() {
+// /**
+//  * Makes circle with canvas that for now colors it in proportion
+//  *   amount of the total interest and the amount of balance. Also,
+//  *   it attaches itself to the canvas with id "canvas."
+//  */
+// 	var canvas = document.getElementById("canvas");
+// 	var ctx = canvas.getContext("2d"),
+// 		width = 700,
+// 		height = 320,
+// 		radius = 160,
+// 		pi = 2 * Math.PI,
+// 		startingBalance = p.getPayObjValue(1, "Starting Balance"),
+// 		totalInterest = p.getPayObjValue("end", "Total Interest"),
+// 		center = Math.floor(height/2);
 
-	canvas.width = width;
-	canvas.height = height;
-	portion = startingBalance / (startingBalance + totalInterest);
+// 	canvas.width = width;
+// 	canvas.height = height;
+// 	portion = startingBalance / (startingBalance + totalInterest);
 
-	function drawCircle(x,y,radius, dataObj, ctx) {
-		var total = 0,
-			colors = Object.keys(dataObj),
-			pi = 2 * Math.PI,
-			prev = "";
+// 	function drawCircle(x,y,radius, dataObj, ctx) {
+// 		var total = 0,
+// 			colors = Object.keys(dataObj),
+// 			pi = 2 * Math.PI,
+// 			prev = "";
 
-		each(dataObj, function(value) { total += value; })
+// 		each(dataObj, function(value) { total += value; })
 		
-		for (var i = 0, len = colors.length; i < len; i++) {
-			var color = colors[i],
-				portion = dataObj[color] / total * pi;
-			if (i === 0) {
-				drawCirclePortion(x, y, radius, 0, portion, color, ctx);
-				prev = portion;
-			} else if (i === len - 1) {
-				drawCirclePortion(x, y, radius, prev, 0, color, ctx);
-			} else {
-				drawCirclePortion(x, y, radius, prev, pi - portion, color, ctx);
-				prev = pi - portion;
-			}
-		}
-	}
+// 		for (var i = 0, len = colors.length; i < len; i++) {
+// 			var color = colors[i],
+// 				portion = dataObj[color] / total * pi;
+// 			if (i === 0) {
+// 				drawCirclePortion(x, y, radius, 0, portion, color, ctx);
+// 				prev = portion;
+// 			} else if (i === len - 1) {
+// 				drawCirclePortion(x, y, radius, prev, 0, color, ctx);
+// 			} else {
+// 				drawCirclePortion(x, y, radius, prev, pi - portion, color, ctx);
+// 				prev = pi - portion;
+// 			}
+// 		}
+// 	}
 
-	function drawCirclePortion(x,y,radius,start,end,color, ctx){
-		ctx.save();
-		ctx.moveTo(center, center);
-		ctx.beginPath();
-		ctx.arc(x, y, radius, start, end, false);
-		ctx.lineTo(center, center);
-		ctx.closePath();
-		ctx.fillStyle = color;
-		ctx.fill();
-		ctx.restore();
-	}
+// 	function drawCirclePortion(x,y,radius,start,end,color, ctx){
+// 		ctx.save();
+// 		ctx.moveTo(center, center);
+// 		ctx.beginPath();
+// 		ctx.arc(x, y, radius, start, end, false);
+// 		ctx.lineTo(center, center);
+// 		ctx.closePath();
+// 		ctx.fillStyle = color;
+// 		ctx.fill();
+// 		ctx.restore();
+// 	}
 
-	drawCircle(center, center, radius, {"green": startingBalance, "red": totalInterest}, ctx);
+// 	drawCircle(center, center, radius, {"green": startingBalance, "red": totalInterest}, ctx);
 
-	// Insert key
-	ctx.textBaseline = "top";
-	ctx.fillStyle = "green";
-	ctx.fillRect(center + radius + 20, height / 15, 20, 20);
-	ctx.font = "16px serif";
-	ctx.fillText("Total Balance Paid: $" + makeCommas(String(startingBalance)) , center + radius + 45, height / 15);
-	ctx.fillStyle = "red";
-	ctx.fillRect(center + radius + 20, height / 15 + 25, 20, 20);
-	ctx.font = "16px serif";
-	ctx.fillText("Total Interest Paid: $" + makeCommas(String(totalInterest)) , center + radius + 45, height / 15 + 25);
-}
+// 	// Insert key
+// 	ctx.textBaseline = "top";
+// 	ctx.fillStyle = "green";
+// 	ctx.fillRect(center + radius + 20, height / 15, 20, 20);
+// 	ctx.font = "16px serif";
+// 	ctx.fillText("Total Balance Paid: $" + makeCommas(String(startingBalance)) , center + radius + 45, height / 15);
+// 	ctx.fillStyle = "red";
+// 	ctx.fillRect(center + radius + 20, height / 15 + 25, 20, 20);
+// 	ctx.font = "16px serif";
+// 	ctx.fillText("Total Interest Paid: $" + makeCommas(String(totalInterest)) , center + radius + 45, height / 15 + 25);
+// }
 
 function decimalConverter(value) {
-	// Makes number into correct format for money
-	// Chose ceil, because money esp. money you owe is
-	// usually round up.
-	return Math.ceil(parseFloat(value) * 100) / 100
+  // Makes number into correct format for money
+  // Chose ceil, because money esp. money you owe is
+  // usually round up.
+  return Math.ceil(parseFloat(value) * 100) / 100
 }
 
-function makeCommas(value) {
-	if (typeof value == 'number') {
-		value = String(value);
-	}
-	return value.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
-}
+// function makeCommas(value) {
+// 	if (typeof value == 'number') {
+// 		value = String(value);
+// 	}
+// 	return value.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+// }
 
-function hideInput(bool) {
-/* For now there are only two input forms, so if status if true
-	then the inputForm will be hidden and optionsForm will show.
-	If false, then the opposite.
-*/
-	var input = document.getElementById("inputForm"),
-		options = document.getElementById("optionsForm");
+// function hideInput(bool) {
+// /* For now there are only two input forms, so if status if true
+// 	then the inputForm will be hidden and optionsForm will show.
+// 	If false, then the opposite.
+// */
+// 	var input = document.getElementById("inputForm"),
+// 		options = document.getElementById("optionsForm");
 
-	if (bool) {
-		input.hidden = true;
-		options.hidden = false;
-	} else {
-		input.hidden = false;
-		options.hidden = true;
-	}
-}
+// 	if (bool) {
+// 		input.hidden = true;
+// 		options.hidden = false;
+// 	} else {
+// 		input.hidden = false;
+// 		options.hidden = true;
+// 	}
+// }
 
 function each(obj, callback) {
 	if (Object.prototype.toString.call(obj) == '[object Object]') {
