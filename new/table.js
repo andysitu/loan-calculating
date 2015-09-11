e2034fbb5edc9224ac84b2c66c04ccb83b583401
Andy Situ <and.situ@gmail.com>
2015-09-11 15:43:56 -0700

    move toggleSelected to tableSelect

--- new/table.js
+++ new/table.js
@@ -21,61 +21,14 @@ var table = {
 
     table.appendChild(tr);
 
-    // should throw error if the first element is accessed
-    //selectedArr[0] = null;
-
     var paymentArray = pObj.getPaymentArray();
 
     each(paymentArray, function(paymentObj, i, pArray) {
       var tr = this.makeRow(paymentObj, headerList);
       table.appendChild(tr);
     }, this)
-
-    // function toggleSelected(e) {
-    //   var ele = e.target
-
-    //   try {
-    //     // accesses the live HTMLCollection of child elements
-    //     // inefficient, but only solution I can think of for now
-    //     // and should be okay for now since no other things are running
-    //       if (ele.children[0].type == "checkbox") {
-    //         var ele = ele.children[0];
-    //         if (ele.checked) {
-    //           ele.checked = false;
-    //         } else {
-    //           ele.checked = true;
-    //         }
-    //       }
-    //   } catch (e) { /*blank*/}
-
-    //   if (ele.type == "checkbox") {
-    //     if (ele.checked) {
-    //       t.changeSelectedList(ele.value, ele.checked);
-    //       var row = document.getElementById("row" + ele.value);
-          
-    //       var children = row.children;
-    //       for (var i = 0, len = row.children.length; i < len; i++) {
-    //         children[i].classList.add("selected");
-    //       }
-    //     } else {
-    //       t.changeSelectedList(ele.value, ele.checked);
-    //       var row = document.getElementById("row" + ele.value);
-          
-    //       var children = row.children;
-    //       for (var i = 0, len = row.children.length; i < len; i++) {
-    //         children[i].classList.remove("selected");
-    //       }
-    //     }
-    //   }
-
       
-    // }
-
-    // table.addEventListener("click", toggleSelected );
-
-    // toggleSelected = null;
-
-    // this.selectedList = selectedArr;
+    table.addEventListener("click", tableSelect.toggleSelected.bind(tableSelect));
 
     return docFrag;
   },
@@ -141,4 +94,5 @@ var table = {
     element.id = id;
     return element;
   }
-}
+
+};
---
+++ new/tableselect.js
@@ -0,0 +1,41 @@
+var tableSelect = {
+  // this.selectedList
+  toggleSelected: function(e) {
+    var ele = e.target
+    console.log(e, ele, this);
+
+    // try {
+    //   // accesses the live HTMLCollection of child elements
+    //   // inefficient, but only solution I can think of for now
+    //   // and should be okay for now since no other things are running
+    //   if (ele.children[0].type == "checkbox") {
+    //     var ele = ele.children[0];
+    //     if (ele.checked) {
+    //       ele.checked = false;
+    //     } else {
+    //       ele.checked = true;
+    //     }
+    //   }
+    // } catch (e) { /*blank*/}
+
+  //   if (ele.type == "checkbox") {
+  //     if (ele.checked) {
+  //       t.changeSelectedList(ele.value, ele.checked);
+  //       var row = document.getElementById("row" + ele.value);
+        
+  //       var children = row.children;
+  //       for (var i = 0, len = row.children.length; i < len; i++) {
+  //         children[i].classList.add("selected");
+  //       }
+  //     } else {
+  //       t.changeSelectedList(ele.value, ele.checked);
+  //       var row = document.getElementById("row" + ele.value);
+        
+  //       var children = row.children;
+  //       for (var i = 0, len = row.children.length; i < len; i++) {
+  //         children[i].classList.remove("selected");
+  //       }
+  //     }
+  //   }
+  }
+}
