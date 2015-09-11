942fd03ad3797ed28977e6bcd75f8fd7cecf5e7f
Andy Situ <and.situ@gmail.com>
2015-09-11 16:31:48 -0700

    (de)selecting sets values in selected

--- new/tableselect.js
+++ new/tableselect.js
@@ -8,41 +8,38 @@ var tableSelect = {
     this.selected.splice(index, 1);
   },
   toggleSelected: function(e) {
-    var ele = e.target
-    console.log(e, ele, this);
+    var ele = e.target;
 
-    // try {
-    //   // accesses the live HTMLCollection of child elements
-    //   // inefficient, but only solution I can think of for now
-    //   // and should be okay for now since no other things are running
-    //   if (ele.children[0].type == "checkbox") {
-    //     var ele = ele.children[0];
-    //     if (ele.checked) {
-    //       ele.checked = false;
-    //     } else {
-    //       ele.checked = true;
-    //     }
-    //   }
-    // } catch (e) { /*blank*/}
+    try {
+      // if user clicks on the td containing checkbox
+      if (ele.children[0].type == "checkbox") {
+        var ele = ele.children[0];
+        if (ele.checked) {
+          ele.checked = false;
+        } else {
+          ele.checked = true;
+        }
+      }
+    } catch (e) { /*blank*/}
 
-  //   if (ele.type == "checkbox") {
-  //     if (ele.checked) {
-  //       t.changeSelectedList(ele.value, ele.checked);
-  //       var row = document.getElementById("row" + ele.value);
+    if (ele.type == "checkbox") {
+      if (ele.checked) {
+        this.select(ele.value);
+        // var row = document.getElementById("row" + ele.value);
         
-  //       var children = row.children;
-  //       for (var i = 0, len = row.children.length; i < len; i++) {
-  //         children[i].classList.add("selected");
-  //       }
-  //     } else {
-  //       t.changeSelectedList(ele.value, ele.checked);
-  //       var row = document.getElementById("row" + ele.value);
+        // var children = row.children;
+        // for (var i = 0, len = row.children.length; i < len; i++) {
+        //   children[i].classList.add("selected");
+        // }
+      } else {
+        this.deselect(ele.value);
+        // var row = document.getElementById("row" + ele.value);
         
-  //       var children = row.children;
-  //       for (var i = 0, len = row.children.length; i < len; i++) {
-  //         children[i].classList.remove("selected");
-  //       }
-  //     }
-  //   }
+        // var children = row.children;
+        // for (var i = 0, len = row.children.length; i < len; i++) {
+        //   children[i].classList.remove("selected");
+        // }
+      }
+    }
   }
 }
