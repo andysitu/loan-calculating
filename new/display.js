45d8d1d234cc6833e5bc3b933006853d57efcc0e
Andy Situ <and.situ@gmail.com>
2015-09-11 13:54:14 -0700

    display.js

--- new/display.js
+++ new/display.js
@@ -1,3 +1,11 @@
 display = {
-  
+  addTable(docFrag) {
+    var test = document.getElementById("table");
+    var tableHere = document.getElementById("tableHere");
+    if (test === null) {
+      tableHere.appendChild(docFrag);
+    } else {
+      tableHere.replaceChild(docFrag, test);
+    }
+  }
 }
