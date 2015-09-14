021a2e9303c14b3ba61b870fc65051d5f874eae6
Andy Situ <and.situ@gmail.com>
2015-09-13 20:10:42 -0700

    add comments to table select

--- new/tableselect.js
+++ new/tableselect.js
@@ -1,13 +1,18 @@
 var tableSelect = {
+// Handles when the user clicks on the table, which
+//  select that payment object. Returns a list of selected.
   selected: [],
   select: function(month) {
+  // Adds selected values to selected.
     this.selected.push(month);
   },
   deselect: function(month) {
+  // Removes values from selected.
     var index = this.selected.indexOf(month);
     this.selected.splice(index, 1);
   },
   getSelected: function() {
+  // Returns the list of selected.
     var selected = copy(this.selected);
     this.selected = [];
     return selected;
@@ -15,8 +20,11 @@ var tableSelect = {
   toggleSelected: function(e) {
     var ele = e.target;
 
+    // If user clicks on header, then end function.
     if (ele.parentNode.id == "Header") {
       return 0;
+    // If user clicks on the the row (th). Changes
+    //  checkbox as a result of the selection.
     } else if (ele.parentNode.tagName == 'TR') {
       var tr = ele.parentNode;
       var checkbox = tr.children[0].children[0];
@@ -25,10 +33,12 @@ var tableSelect = {
       } else {
         checkbox.checked = true;
       }
+    // If the user clicks on the checkboxo.
     } else if (ele.parentNode.parentNode.tagName == 'TR') {
       var tr = ele.parentNode.parentNode;
     }
 
+    // Adds the selected to this.selected.
     var idNumber = parseInt(tr.id.match(/row (\d*)/)[1]);
     if (this.selected.indexOf(idNumber) == -1) {
       this.select(idNumber);
