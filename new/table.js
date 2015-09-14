4710eb699cd46f862bc79d369341b397c0eef700
Andy Situ <and.situ@gmail.com>
2015-09-13 20:24:42 -0700

    commments for table

--- new/table.js
+++ new/table.js
@@ -1,22 +1,17 @@
 var table = {
   minWidths: null,
   createTable: function(paymentSystem) {
-  /* Makes a table to doc by the headerObj (array) containing table heading
-   * to specify order of the headings (optional) and dataObj which is objs 
-   * in array that will contain the actual data.
-   *
-   * If headObj is not provided, then the headings might not be in order
-   * 
-   * Has an event handler for the entire table. Currently, only registers
-   * if user has clicked on the checked input
+  /* Obtains a copy of the payment array. Then, reads it
+   *  and will create a table by forming the header list
+   *  and using lots of helper functions to create the parts.
    */
     var docFrag = document.createDocumentFragment(),
       tableId = "paymentTable";
 
+    // payment array has stringified values.
     var pArrayWithString = this.makePArrayWithString(paymentSystem);
 
     this.minWidths = this.getMinWidths(pArrayWithString); 
-      // obj with headers as keys
 
     var table = this.makeTable(tableId);
     docFrag.appendChild(table);
@@ -25,6 +20,8 @@ var table = {
 
     table.appendChild(this.makeTrWithHeaders(headerList));
 
+    // Goes through payment array to reach the payment objects
+    //  and creates the rows (tr) for the table.
     each(pArrayWithString, function(paymentObj, i, pArray) {
       var tr = this.makeRow(paymentObj, headerList, i);
       table.appendChild(tr);
@@ -42,10 +39,14 @@ var table = {
   },
 
   makeTable: function(id) {
+  // Makes the table element and returns it.
     var table = this.makeElement("table", id);
     return table;
   },
   makeRow: function(dataObj, headerList, month_0) {
+  // Returns tr element and makes it in the order of 
+  //  the headerlist. Also, creates the td elements 
+  //  and appends it. Tr with headers is made in makeTable.
     var tr = this.makeTr("row " + (month_0 + 1));
     each(headerList, function(header, i, headerList) {
       var td = this.makeTd();
@@ -60,10 +61,12 @@ var table = {
     return tr;
   },
   makeTr: function(id) {
+  // Returns tr element.
     var tr = this.makeElement("tr", id);
     return tr;
   },
   makeTrWithHeaders: function(headerList) {
+  // Used in makeTable function.
     var tr = this.makeTr("Header");
     each(headerList, function(head, i, list) {
       var th = this.makeTh(head);
@@ -72,11 +75,13 @@ var table = {
     return tr;
   },
   makeTd: function(content, id) {
+  // Returns td element.
     var td = this.makeElement("td", id);
     td.textContent = content;
     return td;
   },
   makeTdWithCheckbox: function(value, id) {
+  // Returns td element with a checkbox as child node
     var td = this.makeTd();
     var check = this.makeElement("input", id);
     check.type = "checkbox";
@@ -85,11 +90,13 @@ var table = {
     return td;
   },
   makeTh: function(content, id) {
+  // Returns an th element.
     var th = this.makeElement("th", id);
     th.textContent = content;
     return th;
   },
   makeElement: function(type, id) {
+  // Returns an element and appends an id.
     if (id === undefined)
       id = "";
     var element = document.createElement(type);
@@ -97,6 +104,9 @@ var table = {
     return element;
   },
   cellFormatter: function(value, header) {
+  // Correctly forms the string (value) by getting
+  // the minimum width throughout entire payment array
+  // and adds the correct number of spaces and appends '$'.
     if (header == "Month")
       return value;
     else {
@@ -105,6 +115,8 @@ var table = {
     }
   },
   getMinWidths: function(pArrayWithString) {
+  // Gets the minimum width of each column by searching
+  //  the entire payment array.
     var obj = {};
 
     each(pArrayWithString[0], function(_, header) {
@@ -118,6 +130,8 @@ var table = {
     return obj;
   },
   makePArrayWithString(paymentSystem){
+  // Stringifies the values of the payment array
+  //  and returns it.
     var paymentArray = paymentSystem.getPaymentArray();
     paymentArray = map(paymentArray, function(paymentObject, i) {
       return map(paymentObject, function(value, header) {
